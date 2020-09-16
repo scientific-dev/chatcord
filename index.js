60a8f84cb469bcc77ae35b5b7a48b9175a95a462
Science Spot <69678617+Scientific-Guy@users.noreply.github.com>
2020-09-16 23:52:46 +0530

    Under Testing: v0.0.4

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ class Client{
     if(options.sqlite){
       db.options({filename: 'chat.sqlite'})
       this.push = (content) => {
+        content = content.split('"').join("'")
         db.push('chat', content)
         this.chats.push(content)
       }
